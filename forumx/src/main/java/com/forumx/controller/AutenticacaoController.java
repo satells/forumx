f9e2f5025ea8850c3a09bf841dc475fae7f4e29b
satells <msergiost@hotmail.com>
2022-09-02 09:42:31 -0300

    .

--- forumx/src/main/java/com/forumx/controller/AutenticacaoController.java
+++ forumx/src/main/java/com/forumx/controller/AutenticacaoController.java
@@ -1,7 +1,5 @@
 package com.forumx.controller;
 
-import java.util.ArrayList;
-
 import javax.validation.Valid;
 
 import org.springframework.beans.factory.annotation.Autowired;
@@ -34,8 +32,6 @@ public class AutenticacaoController {
 		try {
 			System.out.println(form.getEmail());
 			System.out.println(form.getSenha());
-
-			ArrayList<String> arrayList = new ArrayList<String>();
 
 			UsernamePasswordAuthenticationToken dadosLogin = form.converter();
 			Authentication authentication = authenticationManager.authenticate(dadosLogin);
