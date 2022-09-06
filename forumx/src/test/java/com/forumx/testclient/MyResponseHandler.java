5094eed613352b2d8c01e387c05410bb3c015570
satells <msergiost@hotmail.com>
2022-09-06 15:12:23 -0300

    .

--- forumx/src/test/java/com/forumx/testclient/MyResponseHandler.java
+++ forumx/src/test/java/com/forumx/testclient/MyResponseHandler.java
@@ -4,7 +4,6 @@ import java.io.IOException;
 
 import org.apache.hc.client5.http.ClientProtocolException;
 import org.apache.hc.core5.http.ClassicHttpResponse;
-import org.apache.hc.core5.http.Header;
 import org.apache.hc.core5.http.HttpEntity;
 import org.apache.hc.core5.http.HttpStatus;
 import org.apache.hc.core5.http.ParseException;
@@ -16,22 +15,11 @@ class MyResponseHandler implements HttpClientResponseHandler<String> {
 	public String handleResponse(final ClassicHttpResponse response) throws IOException {
 
 		final int status = response.getCode();
-		String protocol = response.getVersion().format();
 
 		if (status >= HttpStatus.SC_SUCCESS && status < HttpStatus.SC_REDIRECTION) {
 			final HttpEntity entity = response.getEntity();
 			try {
-				StringBuilder sb = new StringBuilder();
-				sb.append(protocol + " " + status + "\n");
-				Header[] headers = response.getHeaders();
-				for (Header header : headers) {
-					sb.append(header.getName() + ": " + header.getValue()).append("\n");
-				}
 				String string = entity != null ? "\n" + EntityUtils.toString(entity) : null;
-				System.out.println(">>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>");
-				sb.append(string);
-				System.out.println(sb);
-				System.out.println(">>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>");
 
 				return string;
 			} catch (final ParseException ex) {
--- forumx/src/test/java/com/forumx/testclient/TestClient.java
+++ forumx/src/test/java/com/forumx/testclient/TestClient.java
@@ -2,6 +2,7 @@ package com.forumx.testclient;
 
 import java.io.IOException;
 
+import org.apache.hc.client5.http.classic.methods.HttpDelete;
 import org.apache.hc.client5.http.classic.methods.HttpPost;
 import org.apache.hc.client5.http.fluent.Request;
 import org.apache.hc.client5.http.impl.classic.CloseableHttpClient;
@@ -13,21 +14,39 @@ import com.fasterxml.jackson.databind.ObjectMapper;
 import com.forumx.controller.form.TopicoForm;
 
 public class TestClient {
+
 	private static final String TOKEN_MODERADOR = "eyJhbGciOiJIUzUxMiJ9.eyJpc3MiOiJBUEkgZG8gRsOzcnVtIFgiLCJzdWIiOiIyIiwiaWF0IjoxNjYyNDY3MzM0LCJleHAiOjE2NjI1NTM3MzR9.X-9sfNno2OSuj6CjsZ2JZxo5r25CSU4IBSgTQBM6iB94CgHUPS-rDcJkbVuyY-xjSmG6eXfLj8fWqXj82AEDkQ";
 
 	public static void main(String[] args) throws Exception {
-		post();
+
+//		delete();
+//		post();
 
 		get();
 
 	}
 
+	private static void delete() throws Exception {
+
+		try (CloseableHttpClient httpclient = HttpClients.createDefault()) {
+			HttpDelete httppost = new HttpDelete("http://localhost:8080/topicos/3");
+			httppost.setHeader("Authorization", "Bearer " + TOKEN_MODERADOR);
+
+			System.out.println("Executing request " + httppost.getMethod() + " " + httppost.getUri());
+
+			String resultado = httpclient.execute(httppost, new MyResponseHandler());
+
+			System.out.println(resultado);
+		}
+	}
+
 	private static void post() throws Exception {
 		String topico = new ObjectMapper().writeValueAsString(new TopicoForm("Dúvida Sobre Postman", "Não está autenticando", "Spring Boot"));
+
 		StringEntity stringEntity = new StringEntity(topico, ContentType.APPLICATION_JSON);
 
-		try (final CloseableHttpClient httpclient = HttpClients.createDefault()) {
-			final HttpPost httppost = new HttpPost("http://localhost:8080/topicos");
+		try (CloseableHttpClient httpclient = HttpClients.createDefault()) {
+			HttpPost httppost = new HttpPost("http://localhost:8080/topicos");
 			httppost.setHeader("Authorization", "Bearer " + TOKEN_MODERADOR);
 
 			httppost.setEntity(stringEntity);
@@ -42,7 +61,7 @@ public class TestClient {
 
 	private static void get() throws IOException {
 
-		String conteudo = Request.get("http://localhost:8080/topicos/1436")
+		String conteudo = Request.get("http://localhost:8080/topicos/3")
 
 				.setHeader("Authorization", "Bearer " + TOKEN_MODERADOR)
 
